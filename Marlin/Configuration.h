7ef3a3f0588e0655b18ffcf22a0a43245df1fa84
Alexey <sav@ellips.ru>
2020-04-17 11:25:47 +0500

    Переход на движки TMC2209

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -468,7 +468,7 @@
 
 // Comment the following line to disable PID and enable bang-bang.
 #define PIDTEMP
-#define BANG_MAX 255     // Limits current to nozzle while in bang-bang mode; 255=full current
+#define BANG_MAX 255     // Limits current to nozzle while in bang-bang mode; 255=full current 
 #define PID_MAX BANG_MAX // Limits current to nozzle while PID is active (see PID_FUNCTIONAL_RANGE below); 255=full current
 #define PID_K1 0.95      // Smoothing factor within any PID loop
 #if ENABLED(PIDTEMP)
@@ -1047,14 +1047,14 @@
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR true  //--sav13 (!)
-#define INVERT_Y_DIR false //--sav13 (!)
-#define INVERT_Z_DIR false
+#define INVERT_X_DIR false  //--sav13 (!) --tmc2209 (!)
+#define INVERT_Y_DIR true //--sav13 (!) --tmc2209 (!)
+#define INVERT_Z_DIR true             //--tmc2209 (!)     
 
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true //--sav13 (!)
+#define INVERT_E0_DIR false //--sav13 (!) //--tmc2209 (!)
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
